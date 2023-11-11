0c7af88fae46e66ca91c3f9e2de7e89136d7dc5a
Zafeer Abbasi <zafeerabbasi57@yahoo.com>
2023-11-11 04:19:16 -0800

    beast

--- Learning/random.py
+++ Learning/random.py
@@ -1,10 +1,17 @@
-for item in range(1, 10):
-    print(item)
-    
-random = [["1", "2"], ["3", "4"]]
-random1 = {
-    "Yes" : 1,
-    "NO" : 2
-}
+"""_summary_
+"""
 
-print(type(random1))
+def mutate_me( a_list ):
+    """_summary_
+
+    Args:
+        a_list (_type_): _description_
+    """
+    b_list = [ ]
+    for item in a_list:
+        new_item = item * 2
+        b_list.append( new_item )
+    print( b_list )
+
+
+mutate_me( [ 1, 2, 3, 5, 8, 13 ] )
